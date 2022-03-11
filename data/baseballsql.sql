cf8b193bbb19e1f8a2088aa2d8eb3a07f6d72584
Aurora Larson <roryvick93@gmail.com>
2022-03-10 21:28:35 -0600

    trying to start 8 and had to take a break from seven, off work tomorrow, will be on most of the day

--- data/baseballsql.sql
+++ data/baseballsql.sql
@@ -122,11 +122,16 @@ WHERE wswin = 'Y'
 GROUP BY yearid, name
 ORDER BY MIN(w) ASC;
 1981 is the problem yr
-SELECT yearid, name, MIN(w)
-	SUM(yearid <> '1981') OVER( PARTITION BY (w)) AS highest_wins,
+SELECT SUM(yearid <> '1981') OVER( PARTITION BY (w)) AS highest_wins
 FROM teams AS t
 WHERE wswin = 'Y'  
 GROUP BY yearid, name
 ORDER BY MIN(w) ASC;
+
+
+8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average 
+attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). 
+Only consider parks where there were at least 10 games played. 
+Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
  
